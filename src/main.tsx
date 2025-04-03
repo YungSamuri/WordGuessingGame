e3271afd36971db91220e52c0d2c8da8142b0349
Hyrum Smith <hyrum.d.smith@gmail.com>
2025-04-02 23:25:38 -0600

    Removed App since it served no purpose

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,23 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { Welcome } from './pages/Welcome'
 import { Game } from './pages/Game'
 import { GameOver } from './pages/GameOver'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 const router = createHashRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-			{
-				path: "/",
-				element: <Welcome />,
-			},
-			{
-				path: "/game",
-				element: <Game />,
-			},
-			{
-				path: "/gameover",
-				element: <GameOver />,
-			}
-		]
-    }
+	{
+		path: "/",
+		element: <Welcome />,
+	},
+	{
+		path: "/game",
+		element: <Game />,
+	},
+	{
+		path: "/gameover",
+		element: <GameOver />,
+	}
 ]);
 
 createRoot(document.getElementById('root')!).render(
