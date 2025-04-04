06f9826bdcf4e03f723d1534ec481d210de246dd
hyrum <hyrum.d.smith@gmail.com>
2025-04-03 20:57:21 -0600

    Changed APIs (the old one stopped working...)

--- src/ApiQuery.ts
+++ src/ApiQuery.ts
@@ -1,6 +1,6 @@
 export async function getWord(difficultyOffset: number): Promise<string[]> {
     const length: number = Math.floor(Math.random() * 3) + difficultyOffset;
-    const reponse = await fetch(`https://random-word-api.herokuapp.com/word?length=${length}`);
+    const reponse = await fetch(`https://random-word-api.vercel.app/api?words=1&length=${length}`);
     const data = await reponse.json();
     return data[0].split("");
 }
